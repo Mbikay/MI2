5fe5605976bf501a7a679b89f832bb2ec6996c41
Mbikay <32297109+Mbikay@users.noreply.github.com>
2019-02-14 16:50:01 +0100

    Update vanStyle.js

--- vanStyle.js
+++ vanStyle.js
@@ -1,6 +1,6 @@
-let img = document.getElementById('img-car');
-img.style.opacity = '1.0';
-img.style.transition = 'all 0.3s';
-img.style.transform = 'scale(1.3)';
-img.style.boxShadow = '#888888 5px 5px 10px';
-
+let img = document.querySelector('#img-car');
+let button=document.querySelector('#btn2');
+button.addEventListener('click',function(){
+ if (img.classList.contains('GRAYSCALE')) img.classList.remove('NORMAL'); 
+  else img.classList.add('GRAYSCALE');
+});
