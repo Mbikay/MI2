a6dc624391b7d2db5cd24be0f36694d30d4292ed
Mbikay <32297109+Mbikay@users.noreply.github.com>
2019-02-14 13:00:39 +0100

    Add files via upload

---
+++ vanStyle.js
@@ -0,0 +1,6 @@
+let img = document.getElementById('img-car');
+img.style.opacity = '1.0';
+img.style.transition = 'all 0.3s';
+img.style.transform = 'scale(1.3)';
+img.style.boxShadow = '#888888 5px 5px 10px';
+
