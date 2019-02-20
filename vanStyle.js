47f0fa02d5706498de12bd1dc777c7b644e8f10b
Mbikay <32297109+Mbikay@users.noreply.github.com>
2019-02-20 14:48:01 +0100

    Update vanStyle.js

--- vanStyle.js
+++ vanStyle.js
@@ -1,6 +1,25 @@
 function myFunction(){
     document.getElementById("myImg").style.filter= "grayscale(100%)";
     
-    document.getElementById("myImg").stle.Webkitfilter="grayscale(100%)";
+    document.getElementById("myImg").stle.WebkitFilter="grayscale(100%)";
 }
-
+function Sepia(){
+        document.getElementById("myImg").style.filter= "sepia(100%)";
+    
+    document.getElementById("myImg").stle.WebkitFilter="sepia(100%)";
+}
+function myHue(){
+        document.getElementById("myImg").style.filter= "hue-rotate(90deg)";
+    
+    document.getElementById("myImg").stle.WebkitFilter="hue-rotate(90deg)";
+}
+function myBlur(){
+        document.getElementById("myImg").style.filter= "blur(1.5rem)";
+    
+    document.getElementById("myImg").stle.WebkitFilter="blur(1.5rem)";
+}
+function myNormal(){
+        document.getElementById("myImg").style.filter= "normal(100%)";
+    
+    document.getElementById("myImg").stle.WebkitFilter="normal(100%)";
+}
